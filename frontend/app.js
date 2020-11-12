06333ce1d5e85fb23c4a6aade40bbd6d8f35a9bb
rapulu <rapulu@yahoo.com>
2020-11-12 14:56:26 +0100

    fix redirect bug

--- frontend/app.js
+++ frontend/app.js
@@ -5,6 +5,7 @@ var inputForm = document.getElementById("inputForm");
 inputForm.addEventListener("submit", (e)=>{
   e.preventDefault()
 
+  var httppro = document.location.protocol
   var url = document.location
 
   var inputUrl = document.getElementById("urlid").value;
@@ -19,7 +20,7 @@ inputForm.addEventListener("submit", (e)=>{
     body: JSON.stringify(data),
   }).then(response => response.json()).then(response => {
     console.log('Success:', response["short_url"]);
-    document.getElementById("response").innerHTML = '<a href='+response["short_url"]+' target="_blank">'+response["short_url"]+'</a>'
+    document.getElementById("response").innerHTML = '<a href='+httppro+response["short_url"]+' target="_blank">'+response["short_url"]+'</a>'
   }).catch((error) => {
     console.error('Error:', error);
   });
